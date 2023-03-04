c87e8d9d9cd8a3ac24ccde0da58d3f7f7aa2a963
biel0209 <marcus.melo@dcomp.ufs.br>
2023-03-04 13:58:04 +0000

    atualizacao

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Chat.java
@@ -10,7 +10,7 @@ public class Chat {
     static String user;
     static String receiver;
     static String view = " ";
-    final static String ip_aws = "34.227.93.195";
+    final static String ip_aws = "100.25.19.83";
     public static void main(String[] argv) throws Exception {
         Scanner sc = new Scanner(System.in);
         Emissor emissor = new Emissor();
